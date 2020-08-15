e97e3337ab575439ea3345b5c6fc09fe95d52b4c
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-15 00:05:25 -0700

    added commas to players' earnings entries

--- src/components/TableEntry/index.js
+++ src/components/TableEntry/index.js
@@ -2,13 +2,17 @@ import React from "react"
 
 function TableEntry({ player }) {
 
+    function numberWithCommas(earnings) {
+        return earnings.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
     return (
         <tr>
             <td>{player.playerID}</td>
             <td>{player.name}</td>
             <td>{player.position}</td>
             <td>{player.team}</td>
-            <td>${player.earnings}</td>
+            <td>${numberWithCommas(player.earnings)}</td>
         </tr>
     )
 }
